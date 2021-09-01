d997c430b0655b4a376918f3a7dbb0fc826ad8cc
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-09-01 16:35:08 +0500

    800 MB -> 500 MB

--- app.py
+++ app.py
@@ -12,18 +12,23 @@ app.config['SECRET_KEY'] = 'this_is_secret_key'
 
 @app.route('/', methods=['GET'])
 def index():
+    global NUMBER_OF_ITERATION
+    NUMBER_OF_ITERATION = 0
     return render_template("index.html")
 
 
 # тут реализация идеи сражения человека с ИИ
 @app.route('/mortal_with_ai', methods=['GET', 'POST'])
 def mortal_with_ai():
-    return render_template("mortal_with_ai.html")
+    image = f"img/for_mortal/marked"
+    return render_template("mortal_with_ai.html", image=)
 
 
 # тестирование ИИ
 @app.route('/test_ai', methods=['GET', 'POST'])
 def test_ai():
+    global NUMBER_OF_ITERATION
+    NUMBER_OF_ITERATION = 0
     result = "//-->>-->>-->>//"
     if request.method == "POST":
         try:
@@ -45,5 +50,6 @@ def favicon():
 
 
 if __name__ == '__main__':
+    NUMBER_OF_ITERATION = 0
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
